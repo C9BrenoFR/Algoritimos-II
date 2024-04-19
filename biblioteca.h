313dda8ac69d223205c06532993a64b3f1d636fe
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2024-04-19 20:29:33 -0300

    Atualiza biblioteca de funções

--- biblioteca.h
+++ biblioteca.h
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-void lerVetorInt(int vet[], int n)
+void lerVetorInt(int *vet, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -11,11 +12,22 @@ void lerVetorInt(int vet[], int n)
     cout << endl;
 }
 
-void imprimirVetorInt(int vet[], int n)
+void imprimirVetorInt(int *vet, int n)
 {
+    cout << "vetor de " << n << " numeros :";
     for (int i = 0; i < n; i++)
     {
         cout << vet[i] << " ";
     }
     cout << endl;
+}
+
+int* autoVetorInt(int n)
+{
+    int* vet = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        *(vet + i) = rand() % 100 + 1;
+    }
+    return vet;
 }
